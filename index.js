61ca027b660b2783b09ebd483017326e764059f2
samuel-hansson <675908423@qq.com>
2020-10-21 20:58:22 +0800

    init teaching management system teacher client

---
+++ index.js
@@ -0,0 +1,12 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+//通常electron应用会包含一个窗口
+let mainWindow;
+
+app.on('ready',function(){
+    
+});
