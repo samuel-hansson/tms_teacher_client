4308e9b2428b7ba394cc13774ad468c21f550659
samuel.hansson <675908423@qq.com>
2020-10-28 16:26:55 +0800

    import ipcRerderer

--- renderer.js
+++ renderer.js
@@ -1,5 +1,14 @@
 //在渲染器进程 (网页) 中。
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
+
+const form = document.querySelector('form');
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+  console.log('create student window form submit');
+  const name = document.querySelector('#name').value;
+
+});
+
 console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
 
 ipcRenderer.on('asynchronous-reply', (event, arg) => {
