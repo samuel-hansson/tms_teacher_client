4644db977fce1629fcf6708b29ef2122843bda3e
samuel-hansson <675908423@qq.com>
2020-11-22 21:41:29 +0800

    prepare for get seating charts json from server

--- main.js
+++ main.js
@@ -3,6 +3,50 @@ const url = require('url');
 const path = require('path');
 
 const {app, BrowserWindow, Menu, ipcMain} = electron;
+var fs=require('fs');
+var url=require('url');
+
+
+const server = require('http').createServer(
+    function(req,res){
+        if(req.url!="/favicon.ico"){
+            var urlObj=url.parse(req.url,true,false);
+            console.log(urlObj.pathname);
+            fs.readFile('testSocketClient.html',function(err,data){
+                if(err){
+                    res.writeHead(404);
+                    res.end(JSON.stringify(err));
+                    return;
+                }
+                // console.log(data.toString());
+                //将文件的内容写入res响应对象
+                res.end(data);
+            });
+        }
+        console.log("a request incomming, http server detected!");
+    }
+);
+
+const options = { /* ... */ };
+const io = require('socket.io')(server, options);
+
+io.on('connection', socket => { 
+    /* ... */ 
+    console.log('a user connected'); 
+    // socket.on("input",msg=>{
+    //     console.log(msg);
+    //     io.emit("output", data);
+    // });
+
+    // socket.on("disconnect", () => {
+    //     console.log("user disconnected");
+    // });
+});
+
+server.listen(3050);
+console.log('http server with socket server started!'); 
+
+
 
 /*
 设置环境变量：当程序运行在生产环境中时，设置成production。
